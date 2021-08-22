dee0372afd32c79c3f6a6eb33ed082e1d5754cea
tranngocson210599 <55520239+tranngocson210599@users.noreply.github.com>
2021-08-22 19:59:32 +0700

    Update JavaspringbootApplication.java
    -Add rem for main function

--- src/main/java/com/springboot/Javaspringboot/JavaspringbootApplication.java
+++ src/main/java/com/springboot/Javaspringboot/JavaspringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaspringbootApplication {
-
+        // main function
 	public static void main(String[] args) {
 		SpringApplication.run(JavaspringbootApplication.class, args);
 	}
