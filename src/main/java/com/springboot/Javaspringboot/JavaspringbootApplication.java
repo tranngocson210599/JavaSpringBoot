f5cfeae1ab4be502bc497ce83acb8bb6c4e3a608
tranngocson210599 <dp1.tranngocson99@gmail.com>
2021-08-22 15:39:14 +0700

    Add folder Java Spring Boot

---
+++ src/main/java/com/springboot/Javaspringboot/JavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/Javaspringboot/JavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
