ab98ae31f764a3d6a091ab5e746fa78fd976b251
tranngocson210599 <dp1.tranngocson99@gmail.com>
2021-08-22 21:39:24 +0700

    Add class Person

--- src/main/java/com/springboot/Javaspringboot/model/Person.java
+++ src/main/java/com/springboot/Javaspringboot/model/Person.java
@@ -1,5 +1,19 @@
 package com.springboot.Javaspringboot.model;
 
-public class Person {
+import java.util.UUID;
 
+public class Person {
+ private  UUID id;
+ private  String name;
+ public Person(UUID id,String name)
+ {
+	 this.name=name;
+	 this.id=id;
+ }
+public UUID getId() {
+	return id;
+}
+public String getName() {
+	return name;
+}
 }
